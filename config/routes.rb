378aaf20e8a1d415a8efaf0130b38a3120bce522
Francis Abad <francisongabad@gmail.com>
2019-02-20 17:15:06 +0900

    yelp wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
+
 end
